8712d27ec628e95aa0de40c78ea6a61d3a28ee94
Madhu <114853422+MadhuSuniL@users.noreply.github.com>
2023-01-08 11:34:13 +0530

    Update views.py

--- CVC/views.py
+++ CVC/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render , redirect
 from rest_framework.generics import RetrieveAPIView , GenericAPIView,CreateAPIView
 import random 
-from django.http import HttpResponse
+from django.http import HttpResponse ,JsonResponse
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
-from .extra import random_style
+from .extra import random_style ,check_any4
 
 def home(req):
     return render(req, "index.html")
@@ -12,11 +12,21 @@ def home(req):
 
 def snack(req):
     data_list = random_style()
-    # print(data)
     con = {"style":data_list[0],"snakes":data_list[1],'lifts':data_list[2],'dinos':data_list[3],"snakes_red":data_list[4],'lifts_red':data_list[5],'dinos_red':data_list[6],"set_lift":data_list[7],'set_snake':data_list[8],'set_dino':data_list[9]}
-    
-    # for i, v in con.items():
-    #     print(v)
-    #     print()
         
     return render(req, "snack.html",context=con)
+
+def any4(req):
+        
+    return render(req, "any4.html")
+
+
+def get_positions(req):
+    a = req.GET['1']
+    b = req.GET['2']
+    data = req.GET
+    res = check_any4(data)
+    data = {
+        'res':res  
+        }
+    return JsonResponse(data)
